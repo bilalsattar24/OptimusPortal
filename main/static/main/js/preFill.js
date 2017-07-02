50ad05c738028652af3deb95cc8e3aecca48a10a
bilalsattar24 <bilalsattar24@gmail.com>
2017-07-01 23:44:56 -0700

    auto select day from date for new day

---
+++ main/static/main/js/preFill.js
@@ -0,0 +1,43 @@
+console.log("test")
+$(".newDatePick").change(function() {
+    console.log("test2")
+    var inputDate = String($(".newDatePick").val());
+    console.log(inputDate)
+    var day = String();
+    var year = String();
+    var month = String();
+    var count = 0; 
+    for (i=0; i<inputDate.length; i++) {
+        if (i<4) {
+            year += inputDate[i];
+        }
+        else if (i>4 && i<7) {
+            month += inputDate[i];
+        }
+        else if (i>7) {
+            day += inputDate[i];
+        }
+    }
+    year = parseInt(year);
+    month = parseInt(month)-1;
+    day = parseInt(day);
+    date = new Date(year,month,day);
+    console.log(date);
+    dayNumber = date.getDay()
+    console.log(dayNumber);
+    var days = 
+    [ 
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    console.log(days[dayNumber]);
+    var dayName = days[dayNumber];
+    $(".newDayName").val(dayName);
+})
+    
+
