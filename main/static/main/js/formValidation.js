10e0eb3ad97ce84c52e261b639d24ecd27ac1d36
bilalsattar24 <bilalsattar24@gmail.com>
2017-07-02 12:43:20 -0700

    more form validations

--- main/static/main/js/formValidation.js
+++ main/static/main/js/formValidation.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
     $('input[name="isOff"]').change(function () {
         if( $('input[name="isOff"]').is(':checked') ) {
             $('input[name="numDeliveries"]').hide();
+            $('#isOff').css('color', 'green');
+            $('.numDeliveries').css('color', 'gray');
+            $('.helperName').css('color', 'gray');
+            $('.driverName').css('color', 'gray');
+            $('.storeNum').css('color', 'gray');
+
         }
         else {
             $('input[name="numDeliveries"]').show();
+            $('#isOff').css('color', 'gray');
+            $('.numDeliveries').css('color', 'black');
+            $('.helperName').css('color', 'black');
+            $('.driverName').css('color', 'black');
+            $('.storeNum').css('color', 'black');
         }
     })
 })
