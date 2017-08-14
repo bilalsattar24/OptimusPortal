a7cf17cd6222e8a03343ab5f7874421fcd52632f
bilalsattar24 <bilalsattar24@gmail.com>
2017-08-13 22:01:49 -0400

    started sunday check for calendar select

---
+++ main/static/main/js/reportsPageValidation.js
@@ -0,0 +1,38 @@
+$(".reportsDateSearch").change(function(){
+    var inputDate = String($(".reportsDateSearch"));
+    console.log(inputDate);
+    var day = String();
+    var year = String();
+    var month = String();
+    var count = 0; 
+    for (i=0; i<inputDate.length; i++) {
+        if (i<4) {
+            year += inputDate[i];
+        }
+        else if (i>4 && i<7) {
+            month += inputDate[i];
+        }
+        else if (i>7) {
+            day += inputDate[i];
+        }
+    }
+    year = parseInt(year);
+    month = parseInt(month)-1;
+    day = parseInt(day);
+    date = new Date(year,month,day);
+    console.log(date);
+    dayNumber = date.getDay();
+    console.log(dayNumber);
+    var days = 
+    [ 
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    console.log(days[dayNumber]);
+
+});
